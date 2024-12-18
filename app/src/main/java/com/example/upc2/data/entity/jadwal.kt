f199f9dd4c8fda46b952fe463de47594e74d6be1
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-18 17:02:31 +0700

    Membuat entitas jadwal dokter

---
+++ app/src/main/java/com/example/upc2/data/entity/jadwal.kt
@@ -0,0 +1,15 @@
+package com.example.upc2.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "jadwal")
+data class jadwal(
+    @PrimaryKey
+    val id: String,
+    val NamaDokter: String,
+    val NamaPasien: String,
+    val NoHp : String,
+    val TanggalKonsultasi: String,
+    val Status : String
+)
