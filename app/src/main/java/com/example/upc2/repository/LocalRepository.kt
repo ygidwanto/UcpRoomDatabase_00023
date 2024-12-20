b3a65a6c8562d409b198446af855a76e151ebbef
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-20 19:17:30 +0700

    Membuat locallRepository Jadwal

--- app/src/main/java/com/example/upc2/repository/LocalRepository.kt
+++ app/src/main/java/com/example/upc2/repository/LocalRepository.kt
@@ -1,7 +1,9 @@
 package com.example.upc2.repository
 
 import com.example.upc2.data.dao.dokterDao
+import com.example.upc2.data.dao.jadwalDao
 import com.example.upc2.data.entity.dokter
+import com.example.upc2.data.entity.jadwal
 import kotlinx.coroutines.flow.Flow
 
 //
@@ -18,5 +20,30 @@ class LocalRepositorydokter(
 
     fun getDokter(id: String): Flow<dokter> {
         return dokterDao.getDokter(id)
+    }
+}
+// Repository Jadwal
+
+class LocalRepositoryJadwal(
+    private val jadwalDao: jadwalDao
+) {
+    suspend fun insertJadwal(jadwal: jadwal) {
+        jadwalDao.insertJadwal(jadwal)
+    }
+
+    fun getAllJadwal(): Flow<List<jadwal>> {
+        return jadwalDao.getAllJadwal()
+    }
+
+    fun getJadwalById(id: String): Flow<jadwal> {
+        return jadwalDao.getAllJadwal(id)
+    }
+
+    suspend fun deleteJadwal(jadwal: jadwal) {
+        jadwalDao.deleteJadwal(jadwal)
+    }
+
+    suspend fun updateJadwal(jadwal: jadwal) {
+        jadwalDao.updateJadwal(jadwal)
     }
 }
