746abbfcc66496a9be922e4ad69bd8d04a81f97b
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-23 05:26:09 +0700

    Membuat Fungsi TopAppBar

---
+++ app/src/main/java/com/example/upc2/ui/costumwidget/TopAppBar.kt
@@ -0,0 +1,12 @@
+package com.example.upc2.ui.costumwidget
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+){
+
+}
