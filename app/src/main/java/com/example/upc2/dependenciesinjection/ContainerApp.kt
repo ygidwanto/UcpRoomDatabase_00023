ebff7f0f9cc749f3947b3b183b209c2751917033
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-23 05:59:01 +0700

    Membuat Repository

--- app/src/main/java/com/example/upc2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/upc2/dependenciesinjection/ContainerApp.kt
@@ -1,5 +1,9 @@
 package com.example.upc2.dependenciesinjection
 
-interface InterfaceContainerApp{
+import com.example.upc2.repository.RepositoryDokter
+import com.example.upc2.repository.RepositoryJadwal
 
+interface InterfaceContainerApp{
+    val repositoryDokter: RepositoryDokter
+    val repositoryJadwal: RepositoryJadwal
 }
--- app/src/main/java/com/example/upc2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/upc2/ui/navigation/AlamatNavigasi.kt
@@ -7,8 +7,26 @@ interface AlamatNAvigasi {
         override val route = "dokter"
     }
     object DestinasiDetailDokter : AlamatNAvigasi {
-        override val route = "detaildokter"
-        const val ID_DOKTER = "iddokter"
+        override val route = "detailDokter"
+        const val ID_DOKTER = "idDokter"
         val routeWithArg = "$route/{$ID_DOKTER}"
     }
 }
+
+// Navigasi untuk halaman Jadwal
+object DestinasiJadwal : AlamatNAvigasi {
+    override val route = "jadwal"
+}
+
+object DestinasiDetailJadwal : AlamatNAvigasi {
+    override val route = "detailJadwal"
+    const val ID_JADWAL = "idJadwal"
+    val routeWithArg = "$route/{$ID_JADWAL}"
+}
+
+object DestinasiUpdateJadwal : AlamatNAvigasi {
+    override val route = "updateJadwal"
+    const val ID_JADWAL = "idJadwal"
+    val routeWithArg = "$route/{$ID_JADWAL}"
+}
+
