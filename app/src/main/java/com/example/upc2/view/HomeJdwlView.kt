08328ad3cf84c2d54807144d9441069a2dbd8c23
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-23 23:17:24 +0700

    Membuat Home Jadwal

--- app/src/main/java/com/example/upc2/view/HomeJdwlView.kt
+++ app/src/main/java/com/example/upc2/view/HomeJdwlView.kt
@@ -1,11 +1,11 @@
-package com.example.upc2.ui.view.dokter
+package com.example.upc2.view
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -15,48 +15,48 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.upc2.data.entity.dokter
+import com.example.upc2.data.entity.jadwal
 import com.example.upc2.ui.costumwidget.TopAppBar
-import com.example.upc2.ui.viewmodel.HomeDktrViewModel
-import com.example.upc2.ui.viewmodel.HomeDokterUiState
+import com.example.upc2.ui.viewmodel.HomeJadwalUiState
+import com.example.upc2.ui.viewmodel.HomeJdwlViewModel
 import com.example.upc2.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
-fun HomeDokterView(
-    viewModel: HomeDktrViewModel = viewModel(factory = PenyediaViewModel.Factory),
-    onAddDokter: () -> Unit = {},
+fun HomeJadwalView(
+    viewModel: HomeJdwlViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddJadwal: () -> Unit = {},
     onDetailClick: (String) -> Unit = {},
     modifier: Modifier = Modifier
 ) {
-    val homeUiState by viewModel.homeDokterUiState.collectAsState() // Mengamati UI State
+    val homeUiState by viewModel.homeJadwalUiState.collectAsState() // Mengamati UI State
     val snackbarHostState = remember { SnackbarHostState() } // Snackbar Host
     val coroutineScope = rememberCoroutineScope()
 
     Scaffold(
         topBar = {
             TopAppBar(
-                judul = "Daftar Dokter",
+                judul = "Daftar Jadwal Dokter",
                 showBackButton = false,
                 onBack = {}
             )
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = onAddDokter,
+                onClick = onAddJadwal,
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(16.dp)
             ) {
                 Icon(
                     imageVector = Icons.Default.Add,
-                    contentDescription = "Tambah Dokter"
+                    contentDescription = "Tambah Jadwal"
                 )
             }
         },
         snackbarHost = { SnackbarHost(snackbarHostState) } // Menambahkan Snackbar Host
     ) { innerPadding ->
-        BodyHomeDokterView(
+        BodyHomeJadwalView(
             homeUiState = homeUiState,
             onClick = { id -> onDetailClick(id) },
             snackbarHostState = snackbarHostState,
@@ -67,8 +67,8 @@ fun HomeDokterView(
 }
 
 @Composable
-fun BodyHomeDokterView(
-    homeUiState: HomeDokterUiState,
+fun BodyHomeJadwalView(
+    homeUiState: HomeJadwalUiState,
     onClick: (String) -> Unit = {},
     snackbarHostState: SnackbarHostState,
     coroutineScope: CoroutineScope,
@@ -97,14 +97,14 @@ fun BodyHomeDokterView(
             }
         }
 
-        homeUiState.listDokter.isEmpty() -> {
+        homeUiState.listJadwal.isEmpty() -> {
             // Menampilkan pesan jika data kosong
             Box(
                 modifier = modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
-                    text = "Tidak ada data Dokter",
+                    text = "Tidak ada data Jadwal",
                     fontSize = 18.sp,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(16.dp)
@@ -113,9 +113,9 @@ fun BodyHomeDokterView(
         }
 
         else -> {
-            // Menampilkan daftar dokter
-            ListDokter(
-                listDokter = homeUiState.listDokter,
+            // Menampilkan daftar jadwal
+            ListJadwal(
+                listJadwal = homeUiState.listJadwal,
                 onClick = onClick,
                 modifier = modifier
             )
@@ -124,8 +124,8 @@ fun BodyHomeDokterView(
 }
 
 @Composable
-fun ListDokter(
-    listDokter: List<dokter>,
+fun ListJadwal(
+    listJadwal: List<jadwal>,
     modifier: Modifier = Modifier,
     onClick: (String) -> Unit = {}
 ) {
@@ -134,10 +134,10 @@ fun ListDokter(
             .fillMaxSize()
             .padding(8.dp)
     ) {
-        items(listDokter) { dokter ->
-            CardDokter(
-                dokter = dokter,
-                onClick = { onClick(dokter.id) }
+        items(listJadwal) { jadwal ->
+            CardJadwal(
+                jadwal = jadwal,
+                onClick = { onClick(jadwal.id) }
             )
         }
     }
@@ -145,8 +145,8 @@ fun ListDokter(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CardDokter(
-    dokter: dokter,
+fun CardJadwal(
+    jadwal: jadwal,
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {}
 ) {
@@ -166,7 +166,7 @@ fun CardDokter(
                 Icon(imageVector = Icons.Filled.Person, contentDescription = "")
                 Spacer(modifier = Modifier.width(4.dp))
                 Text(
-                    text = dokter.Nama,
+                    text = jadwal.NamaDokter,
                     fontWeight = FontWeight.Bold,
                     fontSize = 20.sp
                 )
@@ -175,13 +175,15 @@ fun CardDokter(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
                 Spacer(modifier = Modifier.width(4.dp))
                 Text(
-                    text = dokter.Spesialis,
-                    fontWeight = FontWeight.Bold
+                    text = jadwal.TanggalKonsultasi + ", " + jadwal.Status,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
                 )
             }
         }
     }
 }
+
