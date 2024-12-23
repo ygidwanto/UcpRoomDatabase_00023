f84ec47accbf0aaf6a7745a3bac944a8d3eee377
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-23 07:37:09 +0700

    Membuat Container

--- app/src/main/java/com/example/upc2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/upc2/dependenciesinjection/ContainerApp.kt
@@ -1,9 +1,25 @@
-package com.example.upc2.dependenciesinjection
+package com.example.upc2.dependeciesinjection
 
+import android.content.Context
+import com.example.upc2.data.database.rsDatabase
+import com.example.upc2.repository.LocalRepositoryDokter
+import com.example.upc2.repository.LocalRepositoryJadwal
 import com.example.upc2.repository.RepositoryDokter
 import com.example.upc2.repository.RepositoryJadwal
 
-interface InterfaceContainerApp{
+// Interface untuk menyimpan dependensi aplikasi
+interface InterfaceContainerApp {
     val repositoryDokter: RepositoryDokter
     val repositoryJadwal: RepositoryJadwal
+}
+
+// Implementasi container untuk injeksi dependensi
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val repositoryDokter: RepositoryDokter by lazy{
+        LocalRepositoryDokter(rsDatabase.getDataBase(context).dokterDao())
+    }
+
+    override val repositoryJadwal: RepositoryJadwal by lazy {
+        LocalRepositoryJadwal(rsDatabase.getDataBase(context).jadwalDao())
+    }
 }
