f9b0ba3a28412dfbb743d8a6bd6da24ed3052cc5
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-23 20:11:15 +0700

    Membuat PenyediaViewModel

--- app/src/main/java/com/example/upc2/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/upc2/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,32 @@
-package com.example.upc2.viewmodel
+package com.example.upc2.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.upc2.RsApp
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeJdwlViewModel(
+                RsApp().containerApp.repositoryJadwal
+            )
+        }
+        initializer {
+            HomeJdwlViewModel(
+                RsApp().containerApp.repositoryJadwal,
+            )
+        }
+        initializer {
+            UpdateJadwalViewModel(
+                createSavedStateHandle(),
+                RsApp().containerApp.repositoryJadwal,
+            )
+        }
+    }
+}
+
+fun CreationExtras.KrsApp(): RsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as RsApp)
